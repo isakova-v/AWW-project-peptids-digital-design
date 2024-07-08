67674386614e3bdb03d9cda65ea9eb92e2131e5d
isakova.vmphystech.edu <isakova.vmphystech.edu>
2024-07-08 17:50:13 +0300

    test

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-import pandas as pd
+print(df.head(10))
 
-file_path = '/kaggle/input/datacon24-main/DataCon24-main/Datasets/Mixed CPPs/POSEIDON.csv'
-df = pd.read_csv(file_path, sep=',', encoding='unicode_escape', on_bad_lines='skip')
-
-print(df.head())
